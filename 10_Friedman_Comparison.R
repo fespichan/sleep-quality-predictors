bbffd5867dfe0afe238acd1747b40a9f55b8ae66
Fabio Francisco Espichan J <114115130+fespichan@users.noreply.github.com>
2026-03-05 12:29:59 -0500

    Update 10_Friedman_Comparison.R
    # Model comparison (Friedman + post-hoc)

--- 10_Friedman_Comparison.R
+++ 10_Friedman_Comparison.R
@@ -1,1 +1,11 @@
+# Friedman test
+auc_matrix <- cbind(LR = auc_values_logit, RF = auc_values_rf, XGB = auc_values_xgb)
+friedman.test(auc_matrix)
+
+# Post-hoc pairwise
+pairwise.wilcox.test(
+  c(auc_values_logit, auc_values_rf, auc_values_xgb),
+  rep(c("LR", "RF", "XGB"), each = 100),
+  paired = TRUE, p.adjust.method = "bonferroni"
+)
 
